ebfe5ca6883baa58ae300a8d1e6c1934978cd8c8
Cristhian Traversi <crist.trav@gmail.com>
2023-08-30 21:33:56 -0400

    Se agrega la tarea programada de desactivar timbrados vencidos y finalizados

--- src/global/tasks/tasks.module.ts
+++ src/global/tasks/tasks.module.ts
@@ -4,11 +4,13 @@ import { Suscripcion } from '@database/entity/suscripcion.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CuotasScheduleService } from './cuotas-schedule/cuotas-schedule.service';
+import { TimbradosTasksService } from './timbrados-tasks/timbrados-tasks.service';
+import { Timbrado } from '@database/entity/timbrado.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Suscripcion, Cuota, GeneracionCuotas])
+    TypeOrmModule.forFeature([Suscripcion, Cuota, GeneracionCuotas, Timbrado])
   ],
-  providers: [CuotasScheduleService]
+  providers: [CuotasScheduleService, TimbradosTasksService]
 })
 export class TasksModule {}
