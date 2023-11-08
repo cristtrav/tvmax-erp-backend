6a2632b7a69a4b03e82ed17b52df71cee203fda6
Cristhian Traversi <crist.trav@gmail.com>
2023-11-08 00:13:26 -0300

    Se agrega la opcion de establecer la carpeta de certificados SSL por variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import * as fs from 'fs';
 import { join } from 'path';
 
 async function bootstrap() {
+  const certLocation = process.env.SSL_CERT_FOLDER ?? join(__dirname, '..', 'ssl');
   const app = process.env.SERVER_MODE == 'https' 
   ? await NestFactory.create(AppModule, { httpsOptions: {
-      key: fs.readFileSync(join(__dirname, '..', 'ssl', 'privkey.pem')),
-      cert: fs.readFileSync(join(__dirname, '..', 'ssl', 'cert.pem'))
+      key: fs.readFileSync(join(certLocation, 'privkey.pem')),
+      cert: fs.readFileSync(join(certLocation, 'cert.pem'))
     }
   })
   : await NestFactory.create(AppModule);
