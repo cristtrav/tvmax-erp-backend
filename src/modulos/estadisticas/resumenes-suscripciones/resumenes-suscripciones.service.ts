0da2937cc7f9ef9f7db3c05a379e1f1077ff35c0
Cristhian Traversi <crist.trav@gmail.com>
2024-01-04 01:47:39 -0300

    Version 1.3.2: Refactorizado el codigo del servicio de resumenes de suscripciones: Se utiliza subqueries con ordenamiento por ID

--- src/modulos/estadisticas/resumenes-suscripciones/resumenes-suscripciones.service.ts
+++ src/modulos/estadisticas/resumenes-suscripciones/resumenes-suscripciones.service.ts
@@ -8,20 +8,18 @@ import { ResumenGeneralSuscripcionesDTO } from '@dto/resumen-general-suscripcion
 import { ResumenGruposSuscripcionesDTO } from '@dto/resumen-grupos-suscripciones.dto';
 import { ResumenServiciosSuscripcionDTO } from '@dto/resumen-servicios-suscripcion.dto';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Brackets, Repository, SelectQueryBuilder } from 'typeorm';
+import { Brackets, DataSource, SelectQueryBuilder } from 'typeorm';
 
 @Injectable()
 export class ResumenesSuscripcionesService {
 
     constructor(
-        @InjectRepository(SuscripcionView)
-        private suscripcionViewRepo: Repository<SuscripcionView>
+        private datasource: DataSource
     ){}
 
     private addWhereParamsToQuery(
         alias: string,
-        queryBuilder: SelectQueryBuilder<SuscripcionView>,
+        qb: SelectQueryBuilder<SuscripcionView>,
         queries: {[name: string]: any}
     ): SelectQueryBuilder<SuscripcionView>{
         const {
@@ -38,13 +36,11 @@ export class ResumenesSuscripcionesService {
             iddistrito,
             idbarrio,
             search,
-            sort,
-            offset,
-            limit,
             idcobrador,
             fechainiciocambioestado,
             fechafincambioestado
-        } = queries;        
+        } = queries;
+        let queryBuilder = qb;
         if (eliminado != null) queryBuilder = queryBuilder.andWhere(`${alias}.eliminado = :eliminado`, { eliminado });
 
         if (idcliente)
@@ -91,106 +87,226 @@ export class ResumenesSuscripcionesService {
                 qb = qb.orWhere(`LOWER(${alias}.cliente) LIKE :clisearch`, { clisearch: `%${search.toLowerCase()}%`});
             }));
         }
+        return qb;
+    }
+
+    private addSortOffsetLimitToQuery(
+        alias: string,
+        qb: SelectQueryBuilder<SuscripcionView>,
+        queries: {[name: string]: any}
+    ): SelectQueryBuilder<any>{
+        const { sort, offset, limit } = queries;
+        let queryBuilder = qb;
         if(offset) queryBuilder = queryBuilder.offset(offset);
         if(limit) queryBuilder = queryBuilder.limit(limit);
         if(sort) {
             const sortColumn = sort.substring(1);
             const sortOrder: 'ASC' | 'DESC' = sort.charAt(0) === '-' ? 'DESC' : 'ASC';
-            queryBuilder = queryBuilder.orderBy(`${sortColumn}`, sortOrder);
+            queryBuilder = queryBuilder.orderBy(`${alias}.${sortColumn}`, sortOrder);
         }
-        return queryBuilder;
+        return qb;
     }
 
-    private getCuotasPendientesSelectQuery(queries: { [name: string]: any }): SelectQueryBuilder<SuscripcionView> {
+    private getCuotasPendientesSelectQuery(queries: { [name: string]: any }): SelectQueryBuilder<any> {
         const alias = 'resumen';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.cuotaspendientes`, 'nrocuotas')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.cuotaspendientes', 'cuotaspendientes')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries)
+        }, alias)
         .addGroupBy(`${alias}.cuotaspendientes`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        return this.addSortOffsetLimitToQuery(alias, query, queries);
     }
 
-    private getGruposSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getGruposSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'resumen';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.idgrupo`, 'idgrupo')
         .addSelect(`${alias}.grupo`, 'grupo')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
-        .addGroupBy(`${alias}.idgrupo`)
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.idgrupo', 'idgrupo')
+            .addSelect('s.grupo', 'grupo')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
+        .groupBy(`${alias}.idgrupo`)
         .addGroupBy(`${alias}.grupo`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        if(queries.limit) query = query.limit(queries.limit);
+        if(queries.offset) query = query.offset(queries.offset);
+        if(queries.sort){
+            const sortOrder: 'ASC' | 'DESC' = queries.sort.charAt(0) == '-' ? 'DESC' : 'ASC';
+            const sortColumn = queries.sort.substring(1);
+            if(sortColumn == 'cantidad') query = query.orderBy(`${sortColumn}`, sortOrder);
+            else query = query.orderBy(`${alias}.${sortColumn}`, sortOrder);
+        }
+        return query;
     }
 
-    private getServiciosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getServiciosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'resumen';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.idservicio`, 'idservicio')
         .addSelect(`${alias}.servicio`, 'servicio')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
-        .addGroupBy(`${alias}.idservicio`)
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.idservicio', 'idservicio')
+            .addSelect('s.servicio', 'servicio')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
+        .groupBy(`${alias}.idservicio`)
         .addGroupBy(`${alias}.servicio`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        if(queries.limit) query = query.limit(queries.limit);
+        if(queries.offset) query = query.offset(queries.offset);
+        if(queries.sort){
+            const sortOrder: 'ASC' | 'DESC' = queries.sort.charAt(0) == '-' ? 'DESC' : 'ASC';
+            const sortColumn = queries.sort.substring(1);
+            if(sortColumn == 'cantidad') query = query.orderBy(`${sortColumn}`, sortOrder);
+            else query = query.orderBy(`${alias}.${sortColumn}`, sortOrder);
+        }
+        return query;
     }
 
-    private getEstadosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getEstadosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'suscripcion';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.estado`, 'estado')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.deuda', 'deuda')
+            .addSelect('s.estado', 'estado')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
         .addGroupBy(`${alias}.estado`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        return this.addSortOffsetLimitToQuery(alias, query, queries);
     }
 
-    private getDepartamentosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getDepartamentosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'suscripcion';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.iddepartamento`, 'iddepartamento')
         .addSelect(`${alias}.departamento`, 'departamento')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.iddepartamento', 'iddepartamento')
+            .addSelect('s.departamento', 'departamento')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
         .addGroupBy(`${alias}.iddepartamento`)
         .addGroupBy(`${alias}.departamento`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        if(queries.limit) query = query.limit(queries.limit);
+        if(queries.offset) query = query.offset(queries.offset);
+        if(queries.sort){
+            const sortOrder: 'ASC' | 'DESC' = queries.sort.charAt(0) == '-' ? 'DESC' : 'ASC';
+            const sortColumn = queries.sort.substring(1);
+            if(sortColumn == 'cantidad') query = query.orderBy('COUNT(*)', sortOrder);
+            else query = query.orderBy(`${sortColumn}`, sortOrder); 
+        }
+        return query;
 
     }
 
-    private getDistritosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getDistritosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'suscripcion';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.iddistrito`, 'iddistrito')
         .addSelect(`${alias}.distrito`, 'distrito')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            let subq = subquery
+            .select('s.iddistrito', 'iddistrito')
+            .addSelect('s.distrito', 'distrito')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
         .addGroupBy(`${alias}.iddistrito`)
         .addGroupBy(`${alias}.distrito`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        if(queries.limit) query = query.limit(queries.limit);
+        if(queries.offset) query = query.offset(queries.offset);
+        if(queries.sort){
+            const sortOrder: 'ASC' | 'DESC' = queries.sort.charAt(0) == '-' ? 'DESC' : 'ASC';
+            const sortColumn = queries.sort.substring(1);
+            if(sortColumn == 'cantidad') query = query.orderBy('COUNT(*)', sortOrder);
+            else query = query.orderBy(`${alias}.${sortColumn}`, sortOrder);
+        }
+        return query;
 
     }
 
-    private getBarriosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getBarriosSelectQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'suscripcion';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query: SelectQueryBuilder<any> = this.datasource.manager.createQueryBuilder()
         .select(`${alias}.idbarrio`, 'idbarrio')
         .addSelect(`${alias}.barrio`, 'barrio')
         .addSelect(`COUNT(*)`, 'cantidad')
         .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            const subq = subquery
+            .select('s.idbarrio', 'idbarrio')
+            .addSelect('s.barrio', 'barrio')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC')
+            return this.addWhereParamsToQuery('s', subq, queries);
+        }, alias)
         .addGroupBy(`${alias}.idbarrio`)
         .addGroupBy(`${alias}.barrio`);
-        return this.addWhereParamsToQuery(alias, query, queries);
+        if(queries.limit) query = query.limit(queries.limit);
+        if(queries.offset) query = query.offset(queries.offset);
+        if(queries.sort){
+            const sortOrder: 'ASC' | 'DESC' = queries.sort.charAt(0) == '-' ? 'DESC' : 'ASC';
+            const sortColumn = queries.sort.substring(1);
+            if(sortColumn == 'cantidad') query = query.orderBy('COUNT(*)', sortOrder);
+            else query = query.orderBy(`${alias}.${sortColumn}`, sortOrder);
+        }
+        return query;
     }
 
-    private getResumenGeneralQuery(queries: {[name: string]: any}): SelectQueryBuilder<SuscripcionView>{
+    private getResumenGeneralQuery(queries: {[name: string]: any}): SelectQueryBuilder<any>{
         const alias = 'suscripcion';
-        let query = this.suscripcionViewRepo.createQueryBuilder(alias)
+        let query = this.datasource.manager.createQueryBuilder()
         .select('COUNT(*)', 'cantidadTotal')
         .addSelect(`SUM (CASE WHEN ${alias}.estado = 'C' OR ${alias}.estado = 'R' THEN 1 ELSE 0 END)`, 'cantidadActivos')
         .addSelect(`SUM (CASE WHEN ${alias}.estado = 'D' THEN 1 ELSE 0 END)`, 'cantidadDesconectados')
-        .addSelect(`SUM(${alias}.deuda)`, 'monto');
-        return this.addWhereParamsToQuery(alias, query, queries);
+        .addSelect(`SUM(${alias}.deuda)`, 'monto')
+        .from((subquery) => {
+            let subq = subquery
+            .select('s.estado', 'estado')
+            .addSelect('s.deuda', 'deuda')
+            .from(SuscripcionView, 's')
+            .orderBy('s.id', 'DESC');
+            subq = this.addWhereParamsToQuery('s', subq, queries);
+            return subq;
+        }, alias);
+        return query;
     }
 
     findAllResumenCuotasPendientes(queries: { [name: string]: any }): Promise<ResumenCuotasPendientesSuscripcionesDTO[]> {
@@ -238,7 +354,7 @@ export class ResumenesSuscripcionesService {
     }
 
     async countResumenBarrios(queries: {[name: string]: any}): Promise<number>{
-        return (await (this.getBarriosSelectQuery(queries).offset(0).limit(0).getRawMany())).length;
+        return (await this.getBarriosSelectQuery(queries).offset(0).limit(0).getRawMany()).length;
     }
 
     findAllResumenDepartamentos(queries: {[name: string]: any}): Promise<ResumenDepartamentosSuscripcionesDTO[]>{
