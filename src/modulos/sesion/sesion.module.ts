40ea5a161392ec43789d01737948eeafa4be6518
Cristhian Traversi <crist.trav@gmail.com>
2024-05-21 00:56:27 -0400

    Agregar consultar de roles de usuario en el modulo sesion

--- src/modulos/sesion/sesion.module.ts
+++ src/modulos/sesion/sesion.module.ts
@@ -7,16 +7,21 @@ import { Usuario } from '@database/entity/usuario.entity';
 import { UsuarioView } from '@database/view/usuario.view';
 import { Sesion } from '@database/entity/sesion.entity';
 import { JwtUtilsService } from '@globalutil/jwt-utils.service';
-import { PermisosService } from '@modulos/permisos/permisos.service';
 import { PermisosModule } from '@modulos/permisos/permisos.module';
+import { UsuariosService } from '@modulos/usuarios/usuarios.service';
+import { Rol } from '@database/entity/rol.entity';
+import { RolView } from '@database/view/rol.view';
+import { RolUsuario } from '@database/entity/rol-usuario.entity';
+import { UsuariosModule } from '@modulos/usuarios/usuarios.module';
 
 @Module({
   imports: [
     PermisosModule,
-    TypeOrmModule.forFeature([Usuario, UsuarioView, Sesion]),
+    UsuariosModule,
+    TypeOrmModule.forFeature([Usuario, UsuarioView, Sesion, RolView, RolUsuario, Rol]),
     JwtModule.register({})
   ],
   controllers: [SesionController],
-  providers: [SesionService, JwtUtilsService]
+  providers: [SesionService, JwtUtilsService, UsuariosService]
 })
 export class SesionModule { }
