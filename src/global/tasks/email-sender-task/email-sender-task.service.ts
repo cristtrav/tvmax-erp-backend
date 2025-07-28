1d022ccae480f0b4f7e738961088387972fb5b39
Cristhian Traversi <crist.trav@gmail.com>
2025-07-28 07:32:38 -0300

    feat(email-sender-task): agregar mensaje de correo automatizado

--- src/global/tasks/email-sender-task/email-sender-task.service.ts
+++ src/global/tasks/email-sender-task/email-sender-task.service.ts
@@ -132,7 +132,8 @@ export class EmailSenderTaskService implements OnModuleInit {
             subject: `${tipoDocumentoSubject} Nro ${venta.nrofactura} | ${razonSocial}`,
             text: `Estimado/a ${cliente.razonsocial}, \nAdjunto encontrará su ${tipoDocumentoContent} en formato Documento Tributario Electrónico (DTE) y su versión imprimible (KuDE). \n¡Gracias por su preferencia!`,
             html: `<h3>Estimado/a ${cliente.razonsocial},</h3>
-            <p>Adjunto encontrará su ${tipoDocumentoContent} en formato Documento Tributario Electrónico (DTE) y su versión imprimible (KuDE).</p>
+            <p>Adjunto encontrará su ${tipoDocumentoContent} en formato Documento Tributario Electrónico (DTE) y su versión imprimible (KuDE).</p>            
+            <p>No responder a este correo, es generado automáticamente por el sistema informático.</p>
             <p><strong>¡Gracias por su preferencia!</strong></p>`,
             attachments: [
                 {
